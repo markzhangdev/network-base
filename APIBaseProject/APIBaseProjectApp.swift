fc5eb7861340789d4151ad3b7fb5781185b69085
Ming Zhang <ming.zhang@thelotterycorporation.com>
2023-08-07 10:53:33 +1000

    Init project

---
+++ APIBaseProject/APIBaseProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIBaseProjectApp.swift
+//  APIBaseProject
+//
+//  Created by Zhang, Mark on 7/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct APIBaseProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
